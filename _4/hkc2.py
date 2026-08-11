fd5eb85899dfe8246fe7c77cbc8642077b67e9c6
RetracOff <s490.37977@gmail.com>
2026-08-11 15:14:16 +0300

    Update hkc2.py

--- _4/hkc2.py
+++ _4/hkc2.py
@@ -2,22 +2,13 @@ import requests
 import sys
 
 # URL зеркала сервиса
-base_url = 'https://hixxxx.info'
-success_url = 'https://hixxxx.info/success/'
-
-# Эмуляция реального браузера, чтобы сервер не блокировал скрипт
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Referer': base_url
-}
+base_url = 'https://hide-my-name.me'
+success_url = 'https://hide-my-name.me/success/'
 
 print("|| CRACKED BY LOVEDILKA ❤️‍🔥 ||\n")
 
 # Используем сессию для автоматической поддержки cookies
 session = requests.Session()
-session.headers.update(headers)
 
 try:
     # 1. Запрашиваем главную страницу демо-периода
